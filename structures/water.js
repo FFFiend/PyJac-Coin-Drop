a902b9151942307771dfe712da8acd47d3faa3f3
AlawadhiBOT <90068631+AlawadhiBOT@users.noreply.github.com>
2022-01-19 14:40:38 -0500

    Update water.js

--- structures/water.js
+++ structures/water.js
@@ -1,10 +1,15 @@
 export class Liquid {
     constructor(x, y, w, h, c) {
+        // x and y represent the coordinates of our "particle",
+        // width and height represent its width and height, and
+        // c represents the coefficient of the drag force.
+        // The equation of the force of drag is
+        // F_d = -0.5c*(v^2)*A*v_d
         this.x = x
         this.y = y
-        this.w = w
-        this.h = h
-        this.c = c
+        this.width = w
+        this.height = h
+        this.constant = c
     }
   
     display() {
