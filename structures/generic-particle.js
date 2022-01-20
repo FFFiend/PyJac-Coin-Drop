c331b55392d61344ca4cd840a139de26ebb7c5df
quintik <28828855+quintik@users.noreply.github.com>
2022-01-21 06:30:57 +0800

    generic particle no longer handles applying force

--- structures/generic-particle.js
+++ structures/generic-particle.js
@@ -4,11 +4,6 @@ export class Particle {
         this.location = location;
         this.velocity = this.p.createVector(0, 0);
         this.acceleration = this.p.createVector(0, 0);
-        this.force = 0
-    }
-
-    applyForce(force) {
-        this.force = this.force + force
-        this.acceleration = (force / this.mass) + this.acceleration;
     }
 }
+
