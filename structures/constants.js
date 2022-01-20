5cac5d551a3fee7303fffb903e13b0605178cf1f
quintik <28828855+quintik@users.noreply.github.com>
2022-01-21 01:51:31 +0800

    update constants

---
+++ structures/constants.js
@@ -0,0 +1,20 @@
+// General
+export const GRAVITY = 0.98;
+
+// Pond & Water
+export const POND_COLOR = "#42f569";
+export const WATER_COLOR = "#0000ff";
+export const WATER_RADIUS = 5;
+export const WATER_PARTICLES = 5;
+export const WATER_MASS = 10;
+
+// Coin
+export const DROP_POINT = { x: 300, y: 200 };
+export const COIN_COLOR = "#c9b30e";
+export const COIN_SIZE = { w: 15, h: 3 };
+export const COIN_MASS = 100;
+
+// Air
+export const AIR_PARTICLES = 5;
+export const AIR_MASS = 5;
+export const AIR_RADIUS = 5;
