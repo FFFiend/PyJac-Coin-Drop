ae505b386dcfdecc4a0a3cf40f539d573184078b
quintik <28828855+quintik@users.noreply.github.com>
2022-01-20 18:33:40 +0800

    start implementing world.js functions

--- sketch.js
+++ sketch.js
@@ -1,31 +1,19 @@
-class Vector {
-    constructor(x, y) {
-        this.x = x;
-        this.y = y;
-    }
-
-    add(other) {
-        this.x = this.x + other.x;
-        this.y = this.y + other.y;
-    }
-}
+import { World } from "./structures/world.js";
 
 // Using this pattern prevents p5 from polluting
 // the global namespace.
-const sketch = P5 => {
-    let location = new Vector(50, 50);
+const sketch = p => {
+    const world = new World(p);
 
-    P5.setup = () => {
-        P5.createCanvas(600, 400);
+    p.setup = () => {
+        p.createCanvas(600, 400);
     }
 
-    P5.draw = () => {
-        P5.background("#f2f2f2");
-
-        P5.ellipse(location.x, location.y, 20, 20);
-        let velocity = new Vector(Math.random() * 2, Math.random() * 2);
-        location.add(velocity);
-
+    p.draw = () => {
+        p.background("#f2f2f2");
+        world.drawPond();
+        world.drawWater();
+        world.drawCoin();
     }
 }
 
--- structures/world.js
+++ structures/world.js
@@ -3,8 +3,53 @@
 // useful functionality.
 
 export class World {
-    constructor() {
-        // ...
+    // when creating a new world instance, we give it the
+    // p5 library variable as an argument so we can use
+    // its functions.
+    constructor(p) {
+        this.p = p;
+
+        // constants
+        this.GRAVITY = 0.98;
+        this.DROP_POINT = [300, 200];
+        this.POND_COLOR = "#000000";
+        this.COIN_COLOR = "#c9b30e";
+        this.COIN_SIZE = [15, 3];
+    }
+
+    drawPond() {
+        let w = this.p.width;
+        let h = this.p.height;
+
+        this.p.fill(this.POND_COLOR);
+
+        this.p.beginShape();
+        this.p.vertex(0, h);
+        this.p.vertex(0, h*3/4);
+        this.p.bezierVertex(w/8,      h*3/4,    w/4-20,   h*3/4-30, w/4,   h*3/4);
+        this.p.bezierVertex(w/4+20,   h*15/16,  w*3/4-20, h*15/16,  w*3/4, h*3/4);
+        this.p.bezierVertex(w*3/4+20, h*3/4-30, w*7/8,    h*3/4,    w,     h*3/4);
+        this.p.vertex(w, h);
+        this.p.endShape();
+    }
+
+    drawCoin() {
+        if (DEBUG) this.p.stroke();
+        else this.p.noStroke();
+
+        const [x, y] = this.DROP_POINT;
+        const [w, h] = this.COIN_SIZE;
+
+        this.p.fill(this.COIN_COLOR);
+        this.p.rect(x - w/2, y - h/2, w, h);
+    }
+
+    drawWater() {
+        // pass
+    }
+
+    drawAir() {
+        // pass
     }
 }
 
