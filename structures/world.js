722286b224ad809f6cf512249e9d1b6e285de8ee
quintik <28828855+quintik@users.noreply.github.com>
2022-01-20 09:42:25 +0800

    boilerplate for structures/world

---
+++ structures/world.js
@@ -0,0 +1,10 @@
+// Represents the "world" of the simulation - e.g. handling
+// constants like gravity, drawing the pond, plus some other
+// useful functionality.
+
+export class World {
+    constructor() {
+        // ...
+    }
+}
+
